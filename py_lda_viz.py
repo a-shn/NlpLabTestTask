8d5b2a4fd286ee14f83602e685d60d87af36683c
xiu-xiu <sh.il.almaz@gmail.com>
2020-09-09 14:59:17 +0300

    fix

--- py_lda_viz.py
+++ py_lda_viz.py
@@ -1,12 +1,14 @@
 import pyLDAvis.gensim as gensimvis
 import pyLDAvis
+import gensim
 import csv
 import logging
 
 from gensim.corpora import Dictionary
-from gensim.models import LdaModel
+from gensim.models.wrappers import LdaMallet
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+path_to_mallet_binary = "/home/xiu-xiu/Mallet/bin/mallet"
 
 tweets = []
 with open('data/clear_covid_tweets.csv', newline='') as csvfile:
@@ -16,7 +18,8 @@ with open('data/clear_covid_tweets.csv', newline='') as csvfile:
 
 dictionary = Dictionary(tweets)
 corpus = [dictionary.doc2bow(tweet) for tweet in tweets]
-model = LdaModel(corpus=corpus, num_topics=50, id2word=dictionary)
+model = gensim.models.wrappers.ldamallet.malletmodel2ldamodel(
+	LdaMallet(path_to_mallet_binary, corpus=corpus, num_topics=50, id2word=dictionary))
 
 vis_data = gensimvis.prepare(model, corpus, dictionary)
 res = pyLDAvis.display(vis_data)
